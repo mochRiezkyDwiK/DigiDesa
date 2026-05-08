ec6c9cf4fb1f93192ffe750799a4e64945d67ee9
Sendjaa <dewa4388@gmail.com>
2026-05-08 16:59:40 +0700

    meperbaiki dan menambahkan fitur transaparansi anggaran

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App.tsx'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import './index.css'
+
+function Layout() {
+  return (
+    <div className="min-h-screen bg-white font-sans antialiased">
+      <Navbar />
+      <main className="flex-1">
+        <App />
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Layout />
+    </Router>
   </React.StrictMode>,
 )
